7172828fc36f29f248b662f0721cde94c699e520
someone5678 <nemui3353@gmail.com>
2023-08-12 17:01:41 +0800

    core: Hide ADB and developer setting enable status
    * Some apps check whether ADB or developer setting is enabled
    * Return 0 to selected apps when those apps getInt
      ADB_ENABLED, ADB_WIFI_ENABLED, DEVELOPMENT_SETTINGS_ENABLED
    
    mnri: move to getIntForUser to cover more scenarios
    Change-Id: Ibbf92c7b1f1181d2508cb22ff2fc2017f9e59706
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    [dereference23: Simplified the code]
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4644,6 +4644,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def,
                 @CanBeCURRENT @UserIdInt int userId) {
+            if (shouldHideSetting(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSettingWithDefault(v, def);
         }
@@ -4676,6 +4679,9 @@ public final class Settings {
         public static int getIntForUser(ContentResolver cr, String name,
                 @CanBeCURRENT @UserIdInt int userId)
                 throws SettingNotFoundException {
+            if (shouldHideSetting(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSetting(v, name);
         }
@@ -7660,6 +7666,9 @@ public final class Settings {
         @UnsupportedAppUsage
         public static int getIntForUser(ContentResolver cr, String name, int def,
                 @CanBeCURRENT @UserIdInt int userId) {
+            if (shouldHideSetting(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSettingWithDefault(v, def);
         }
@@ -7691,6 +7700,9 @@ public final class Settings {
         public static int getIntForUser(ContentResolver cr, String name,
                 @CanBeCURRENT @UserIdInt int userId)
                 throws SettingNotFoundException {
+            if (shouldHideSetting(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getStringForUser(cr, name, userId);
             return parseIntSetting(v, name);
         }
@@ -19516,6 +19528,9 @@ public final class Settings {
          * or not a valid integer.
          */
         public static int getInt(ContentResolver cr, String name, int def) {
+            if (shouldHideSetting(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getString(cr, name);
             return parseIntSettingWithDefault(v, def);
         }
@@ -19540,6 +19555,9 @@ public final class Settings {
          */
         public static int getInt(ContentResolver cr, String name)
                 throws SettingNotFoundException {
+            if (shouldHideSetting(cr.getPackageName(), name)) {
+                return 0;
+            }
             String v = getString(cr, name);
             return parseIntSetting(v, name);
         }
@@ -22761,5 +22779,28 @@ public final class Settings {
             return null;
         }
         return packages[0];
+    }
+
+    /**
+     * @hide
+     */
+    public static boolean shouldHideSetting(@NonNull String packageName, @NonNull String name) {
+        switch (name) {
+            case Settings.Global.ADB_ENABLED:
+            case Settings.Global.ADB_WIFI_ENABLED:
+            case Settings.Global.DEVELOPMENT_SETTINGS_ENABLED:
+                return !isCallerSystemApp(packageName);
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * @hide
+     */
+    private static boolean isCallerSystemApp(@NonNull String packageName) {
+        return packageName.equals("android") ||
+               packageName.startsWith("com.android.") ||
+               packageName.equals("com.google.android.settings.intelligence");
     }
 }
