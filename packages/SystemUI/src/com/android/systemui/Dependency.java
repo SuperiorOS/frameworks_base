468a898bbde75d10e95565bd9a896df6be30279e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-22 14:40:52 +0800

    SystemUI: Add ScrimUtils helper [SQUASHED]
    [neobuddy89: Squashed all fixes by same author.]
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/Dependency.java
+++ packages/SystemUI/src/com/android/systemui/Dependency.java
@@ -54,6 +54,7 @@ import com.android.systemui.statusbar.policy.BluetoothController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.window.StatusBarWindowControllerStore;
 import com.android.systemui.tuner.TunerService;
+import com.android.systemui.util.ScrimUtils;
 
 import dagger.Lazy;
 
@@ -150,6 +151,7 @@ public class Dependency {
     @Inject Lazy<UserTracker> mUserTrackerLazy;
     @Inject Lazy<StatusBarWindowControllerStore> mStatusBarWindowControllerStoreLazy;
     @Inject Lazy<SysUIStateDisplaysInteractor> mSysUIStateDisplaysInteractor;
+    @Inject Lazy<ScrimUtils> mScrimUtils;
 
     @Inject
     public Dependency() {
@@ -196,6 +198,8 @@ public class Dependency {
         mProviders.put(SysUIStateDisplaysInteractor.class, mSysUIStateDisplaysInteractor::get);
         mProviders.put(
                 StatusBarWindowControllerStore.class, mStatusBarWindowControllerStoreLazy::get);
+        mProviders.put(
+                ScrimUtils.class, mScrimUtils::get);
 
         Dependency.setInstance(this);
     }
